70dfc52684cf52f927b6e32156aac90a8559fb37
Shang-Lin Chen <shanglin.chen@gmail.com>
2025-06-29 00:29:59 -0700

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -39,11 +39,8 @@ export function Layout({ children, activeTab, onTabChange }: LayoutProps) {
       )}
 
       {/* Sidebar */}
-      /* <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0 flex flex-col`}> */
-        <div className={`z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col
-  ${sidebarOpen ? 'fixed inset-y-0 left-0 translate-x-0' : 'fixed inset-y-0 left-0 -translate-x-full'}
-  lg:static lg:translate-x-0`}>
-
+      <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0 flex flex-col`}>
+        
         {/* Sidebar Header */}
         <div className="flex items-center justify-between h-16 px-6 border-b border-gray-200 flex-shrink-0">
           <div className="flex items-center">
